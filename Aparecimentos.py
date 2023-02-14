1dc5610943c48c642a5ea8ecf368c020f31c8b26
Yan Manica Oehling <125397533+yanoehling@users.noreply.github.com>
2023-02-14 18:49:50 -0300

    Update Aparecimentos.py

--- Aparecimentos.py
+++ Aparecimentos.py
@@ -1,12 +1,15 @@
 from operator import itemgetter
+
 cont = {}
 y = 1
 a = open('jogos.txt')
 num = a.read().replace('\n', ' ').split()
+
 for c in range(1, 61):
     cont[str(c)] = num.count(str(c))
-print(f'{"Pos.":<6}{"Nº":<8}{"Quant."}')
+print(f'{"Rank.":<6}{"Nº":<8}{"Aparecimentos"}')
 print('-' * 21)
+
 for k, v in sorted(cont.items(), key=itemgetter(1), reverse=True):
     print(f'{f"{y}º":<6}\033[33m{k:<9}\033[32m{v}\033[m')
     y += 1
